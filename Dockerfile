675c6191b395e313743ba0204341c41e68ac57f5
goisbrunoaugusto <brunogois902@gmail.com>
2025-04-27 17:44:48 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.13.3-alpine
+
+RUN apk update && apk add --no-cache postgresql-dev gcc python3-dev musl-dev
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ core/core/urls.py
@@ -0,0 +1,23 @@
+
+from django.contrib import admin
+from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="twitthon",
+        default_version='v1',
+        description="Twitter clone made with django",
+        contact=openapi.Contact(email="brunogois902@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger',
+        cache_timeout=0), name='schema-swagger-ui'),
+]
