21293edee7622fca31790d1300d3e57df896a957
goisbrunoaugusto <brunogois902@gmail.com>
2025-04-27 18:40:32 -0300

    feat: registro de usuario

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import RegisterUserView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,4 +9,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterUserView.as_view(), name='register'),
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user).data,
+            "message": "User created successfully"
+        }, status=status.HTTP_201_CREATED)
