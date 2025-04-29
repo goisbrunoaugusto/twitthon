caa44a49093238a0b5aedade06613f9efc1dd815
goisbrunoaugusto <brunogois902@gmail.com>
2025-04-29 19:00:24 -0300

    feat: add media file handling by configuring MEDIA_URL and MEDIA_ROOT

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,3 +24,6 @@ urlpatterns = [
         cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
