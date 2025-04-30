d004ba7dd7a6f47f3655842819ab3e9e34f4702f
goisbrunoaugusto <brunogois902@gmail.com>
2025-04-30 10:05:50 -0300

    feat: add validation to PostSerializer to ensure content or image is provided

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -20,8 +20,16 @@ class UserSerializer(serializers.ModelSerializer):
     
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.CharField(source='author.username', read_only=True)
+    content = serializers.CharField(required=False, allow_blank=True)
+    image = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = Post
         fields = ('id', 'author', 'content', 'image', 'created_at', 'likes')
-        
+    
+    def validate(self, data):
+        content = data.get('content', '')
+        image = data.get('image', None)
+        if not content and not image:
+            raise serializers.ValidationError("Either content or image must be provided.")
+        return data
